6991dd5417219d3013c1c23848da6c3183e63469
angelaslinker <angela.r.slinker@gmail.com>
2023-09-29 17:08:54 -0700

    added validation and fixed bugs

--- routes/makeup.js
+++ routes/makeup.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 const makeupController = require('../controllers/makeup');
+
+// Validation middleware for POST and PUT routes
+const validateMakeup = [
+    body('name').notEmpty().withMessage('Name is required'),
+    body('brand').notEmpty().withMessage('Brand is required'),
+    body('price').isNumeric().withMessage('Price must be a number'),
+    // Add more validation rules for your fields here
+];
+
+// Middleware to handle validation errors
+const handleValidationErrors = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+};
 
 router.get('/', makeupController.getAll);
 
 router.get('/:id', makeupController.getSingle);
 
-router.post('/', makeupController.createMakeup);
+router.post('/', validateMakeup, handleValidationErrors, makeupController.createMakeup);
 
-router.put('/:id', makeupController.updateMakeup);
+router.put('/:id', validateMakeup, handleValidationErrors, makeupController.updateMakeup);
 
 router.delete('/:id', makeupController.deleteMakeup);
 
